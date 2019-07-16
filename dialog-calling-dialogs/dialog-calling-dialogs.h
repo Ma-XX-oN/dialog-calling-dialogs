f4c7b4307c57c99c8db0f81b4691ad2d57af4528
Adrian Hawryluk <adrian.hawryluk@caseware.com>
2019-07-16 18:04:43 -0400

    Add project files.

---
+++ dialog-calling-dialogs/dialog-calling-dialogs.h
@@ -0,0 +1,32 @@
+
+// dialog-calling-dialogs.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CdialogcallingdialogsApp:
+// See dialog-calling-dialogs.cpp for the implementation of this class
+//
+
+class CdialogcallingdialogsApp : public CWinApp
+{
+public:
+	CdialogcallingdialogsApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CdialogcallingdialogsApp theApp;
