e2ea942c2866f4c7f1972a08ac13dd786d73d142
Adrian Hawryluk <adrian.hawryluk@caseware.com>
2019-07-17 10:50:18 -0400

    - Added images to exaserbate the issue.

--- dialog-calling-dialogs/Resource.h
+++ dialog-calling-dialogs/Resource.h
@@ -8,15 +8,17 @@
 #define IDD_DIALOGCALLINGDIALOGS_DIALOG 102
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG_LVL_1                130
-#define IDD_DIALOG2                     131
 #define IDD_DIALOG_LVL_2                131
+#define IDB_TREES                       137
+#define IDB_CITY                        141
+#define IDB_BITMAP2                     142
 #define IDC_DLG_LVL_1                   1000
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_RESOURCE_VALUE        143
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
